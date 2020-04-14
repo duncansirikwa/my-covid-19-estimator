c6dcea166d76ec5b690723e49a77e36108d0afab
Cherogony <duncansirikwa@gmail.com>
2020-04-14 12:04:44 +0300

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,42 +1,42 @@
 const estimateCurrentlyInfected = (impact, data) => {
     const currentlyInfected = Math.trunc(data.reportedCases * impact);
     return currentlyInfected;
-  };
+};
 
 // infectionsByrequestedTime()
 
 const infectionsByrequestedTime = (data, currentlyInfected) => {
-let time = '';
-let infectionBRT = '';
+    let time = '';
+    let infectionBRT = '';
 
-// days
+    // days
 
-if (data.periodType.toLowerCase() === 'days') {
-    time = Math.trunc((data.timeToElapse * 1) / 3);
-}
+    if (data.periodType.toLowerCase() === 'days') {
+        time = Math.trunc((data.timeToElapse * 1) / 3);
+    }
 
-// weeks
+    // weeks
 
-if (data.periodType.toLowerCase() === 'weeks') {
-    time = Math.trunc((data.timeToElapse * 7) / 3);
-}
+    if (data.periodType.toLowerCase() === 'weeks') {
+        time = Math.trunc((data.timeToElapse * 7) / 3);
+    }
 
-// months
+    // months
 
-if (data.periodType.toLowerCase() === 'months') {
-    time = Math.trunc((data.timeToElapse * 30) / 3);
-}
+    if (data.periodType.toLowerCase() === 'months') {
+        time = Math.trunc((data.timeToElapse * 30) / 3);
+    }
 
-infectionBRT = Math.trunc(currentlyInfected * (2 ** time));
-return infectionBRT;
+    infectionBRT = Math.trunc(currentlyInfected * (2 ** time));
+    return infectionBRT;
 
 };
 
 // Severe cases by Requested Time Function
 const estSevereCasesBRT = (infectionByRT) => {
-  const estSevereCasesByRT = Math.trunc(infectionByRT * (15 / 100));
+    const estSevereCasesByRT = Math.trunc(infectionByRT * (15 / 100));
     return estSevereCasesBRT;
-}; 
+};
 
 const estimateImpact = (data, impact) => {
 
@@ -48,7 +48,7 @@ const estimateImpact = (data, impact) => {
     const impactObj = {
         currentlyInfected: currentlyInfectedR,
         infectionsByrequestedTime: infectionByRT,
-        severeCasesByRequestedTime: Math.trunc(severeCasesByRequestedTime), 
+        severeCasesByRequestedTime: Math.trunc(severeCasesByRequestedTime),
     };
     return impactObj;
 };
@@ -56,8 +56,12 @@ const estimateImpact = (data, impact) => {
 const covid19ImpactEstimator = (data) => {
     const impact = estimateImpact(data, 10);
     const severeImpact = estimateImpact(data, 50);
-    const completeData = { data, impact, severeImpact };
+    const completeData = {
+        data,
+        impact,
+        severeImpact
+    };
     return completeData;
-  };
+};
 
 export default covid19ImpactEstimator;
